cbd3a831e26caeaa15a95ab7a2fa6437c0b0d49d
Vladimir Perepelkin <79280805+dogfew@users.noreply.github.com>
2022-08-30 14:30:43 +0300

    Add files via upload

--- scripts/results.js
+++ scripts/results.js
@@ -14,4 +14,4 @@ result = getQueryVariable("result")
 max_result = getQueryVariable("max_result")
 
 let result_label = document.getElementById("result-label")
-result_label.innerHTML = `Ваш результат: ${result} из ${max_result}<br>  ${Math.round(result / max_result * 100)}%`
+result_label.innerHTML += `Ваш результат: ${result} из ${max_result}<br>  ${Math.round(result / max_result * 100)}%`
